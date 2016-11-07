7fd57805aad8631e5196460e546f84fd6741e85e
Luiz Paulo <lindennerd@gmail.com>
2016-11-07 16:26:43 -0200

    le corpo do email e envia

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var ImapClient = require('imap-client');
 var mailreader = require('mailreader');
+var sqlite3 = require('sqlite3').verbose();
+var cheerio = require('cheerio');
+var nodemailer = require('nodemailer');
 
 var imap = new ImapClient({
     port: 993,
@@ -16,31 +19,76 @@ var imap = new ImapClient({
     maxUpdateSize: 20
 });
 
-imap.login().then(function () {
-    console.log('logged');
-    imap.listMessages({
-        path: 'INBOX',
-        firstUid: '64605',
-        lastUid: '*'
-    }).then(function (messages) {
-        //console.log(messages);
-        for (var index in messages) {
-            var email = messages[index];
-            imap.getBodyParts({
-                path: 'INBOX',
-                uid: email.uid,
-                bodyParts: email.bodyParts
-            }).then(function (bodyParts) {
-                mailreader.parse({bodyParts: bodyParts}, function (err, bodyParts) {
-                    console.log(bodyParts[0].content[0]);
-                });
-            })
-        }
+var transporter = nodemailer.createTransport('smtps://lindennerd@gmail.com:574r@w4r5123@smtp.gmail.com');
 
-    }).then(function () {
-        imap.logout();
-    })
+var db = new sqlite3.Database('.\\emailautomation.db');
+initDB(function (lastUid) {
+    imap.login().then(function () {
+        console.log('logged');
+        imap.listMessages({
+            path: 'INBOX',
+            // firstUid: '64605',
+            firstUid: lastUid || '4500',
+            lastUid: '*'
+        }).then(function (messages) {
+            for (var index in messages) {
+                var email = messages[index];
+
+                if (email.subject.indexOf('Typeform') !== -1) {
+                    imap.getBodyParts({
+                        path: 'INBOX',
+                        uid: email.uid,
+                        bodyParts: email.bodyParts
+                    }).then(function (bodyParts) {
+                        mailreader.parse({ bodyParts: bodyParts }, function (err, bodyParts) {
+                            var html = bodyParts.filter(function (part) {
+                                if (part.type === 'html') {
+                                    return part;
+                                }
+                            });
+                            var li = cheerio('li:contains("What is your email address")', html[0].content);
+                            var emailAddress = li.find('a').text();
+                            insert(email.uid, emailAddress, db);
+                            sendEmail(emailAddress);
+                        });
+                    }).then(function () {
+                        imap.logout();
+                        //db.close();
+                    });
+                }
+            }
+        })
+    });
 });
+
+
+
+function initDB(cb) {
+    db.run('create table if not exists emailautomation (lastUid INTEGER, email TEXT)');
+    db.each('select max(lastUid) as lastUid from emailautomation', function (err, row) {
+        cb(row.lastUid);
+    })
+}
+
+function insert(uid, email, db) {
+    var stmt = db.prepare("INSERT INTO emailautomation VALUES (?, ?)");
+    stmt.run(uid, email);
+    stmt.finalize();
+}
+
+function sendEmail(address){
+    var mailOPtions = {
+        from: 'Luiz Paulo <lindennerd@gmail.com>',
+        to: 'lindennerd@gmail.com', //address,
+        subject: 'Hi from Test',
+        text: 'Hello World'
+    };
+
+    transporter.sendMail(mailOPtions, function(err, info){
+        if(err) {console.log(err)}
+        console.log(info);
+    })
+}
 
 // ref https://github.com/whiteout-io/imap-client
 // ref https://www.npmjs.com/package/mailreader
