f1002466fa59663213960b8cdec0ccaa3894f823
Luiz Paulo <lindennerd@gmail.com>
2016-11-07 11:12:47 -0200

    lendo o conteudo do email

--- index.js
+++ index.js
@@ -2,37 +2,42 @@ var ImapClient = require('imap-client');
 var mailreader = require('mailreader');
 
 var imap = new ImapClient({
-    port: 143,
-    host: 'imap.powerbrasil.com.br',
-    secure: false,
+    port: 993,
+    host: 'imap.gmail.com',
+    secure: true,
     ignoreTLS: false,
-    requireTLS: false,
+    requireTLS: true,
+
     auth: {
-        user: 'luiz.paulo@powerbrasil.com.br',
-        pass: 'power100',
+        user: 'lindennerd@gmail.com',
+        pass: '574r@w4r5123',
     },
 
     maxUpdateSize: 20
 });
 
-imap.login().then(function() {
+imap.login().then(function () {
     console.log('logged');
     imap.listMessages({
         path: 'INBOX',
         firstUid: '64605',
         lastUid: '*'
-    }).then(function(messages) {
+    }).then(function (messages) {
         //console.log(messages);
         for (var index in messages) {
             var email = messages[index];
-            if (email.subject.includes('Typeform')) {
-                mailreader.parse(email, function(err, bodyParts) {
+            imap.getBodyParts({
+                path: 'INBOX',
+                uid: email.uid,
+                bodyParts: email.bodyParts
+            }).then(function (bodyParts) {
+                mailreader.parse({bodyParts: bodyParts}, function (err, bodyParts) {
                     console.log(bodyParts[0].content[0]);
                 });
-            }
+            })
         }
 
-    }).then(function() {
+    }).then(function () {
         imap.logout();
     })
 });
